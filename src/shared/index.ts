7afa509404a19218f0642fb6b109b2b17fc88f9c
Sergey Vinogorov <webdev.vinogorov@gmail.com>
2026-03-29 18:56:32 +0300

    ProtectedRoute + useAuth

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -8,3 +8,5 @@ export {
   BaseErrorMessage,
   BaseErrorInputMessage,
 } from './ui/BaseErrorMessage.tsx';
+export type { User, LocationState } from './model/auth';
+export { TOKEN_KEY, API_URLS } from './constants/auth';
